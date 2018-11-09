596f2502abae8b487e7995625d7513d0c430f489
Claire Karban <claire.karban@colorado.edu>
2018-11-09 09:59:11 -0700

    Calculations for Table1

---
+++ ReferenceValues.R
@@ -0,0 +1,18 @@
+reference_dat <- read.csv("ReferenceValues.csv", as.is=TRUE, na.strings = "n/a") #read in data
+
+head(reference_dat)
+
+meansVegCover <- aggregate(reference_dat[, 8:14], list(reference_dat$Site), mean)
+SMmeanSoilCover <- aggregate(SM[, 15:27], list(SM$Site), mean)
+
+
+SM <- subset(reference_dat, Site == "SM")
+library(dplyr)
+SM %>% summarise_each(funs(mean))
+str(SM)
+
+colMeans(SM[sapply(SM, is.character)])
+
+calcSE <- function(x){sd(x)/sqrt(length(x))}
+VegCoverSE <- aggregate(reference_dat[, 8:14], list(reference_dat$Site), calcSE)
+VegCoverSE
