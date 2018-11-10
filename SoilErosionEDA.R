4fadf12956c85951ee2e63c1720dffab3af1f870
Claire Karban <claire.karban@colorado.edu>
2018-11-10 10:59:17 -0700

    Added more plots to look at relationships between variables.

--- SoilErosionEDA.R
+++ SoilErosionEDA.R
@@ -141,3 +141,74 @@ ggplot(data=erosion_dat) +
   labs(x="Year",y="Water Erosion (g/M2)")
 #Notes: Shay has more wind and water erosion than Wray, so the effect of year 1 versus year 2 is more dramatic at Shay.
 
+###Finally, graph predictor variables with response variable
+#Wind Erosion
+ggplot(data=BSNE_dat) +
+  geom_point(mapping = aes(x=Median_SoilStability, y=log(BSNE_gM2day+.1), col=Site), position="jitter") #Not a tight relationship, but looks negative and linear
+
+ggplot(data=BSNE_dat) +
+  geom_point(mapping = aes(x=VegCover, y=log(BSNE_gM2day+.1), col=Site), position="jitter") #This is a better relationship; negative and linear
+
+ggplot(data=BSNE_dat) +
+  geom_point(mapping = aes(x=Chla, y=log(BSNE_gM2day+.1), col=Site), position="jitter") #There looks like NO relationship here for WM, and maybe a weakly negative one for SM
+ggplot(data=BSNE_dat) +
+  geom_point(mapping = aes(x=Chla, y=BSNE_gM2day, col=Site), position="jitter") #This non-log doesn't help much.
+
+
+ggplot(data=BSNE_dat) +
+  geom_point(mapping = aes(x=P_Grasses._Sedges, y=log(BSNE_gM2day+.1), col=Site), position="jitter") #Not a strong relationship; maybe stronger for WM than SM
+ggplot(data=BSNE_dat) +
+  geom_point(mapping = aes(x=P_Grasses._Sedges, y=BSNE_gM2day, col=Site), position="jitter") #Without the log, it looks like more of a relationship, especially for SM!
+
+
+ggplot(data=BSNE_dat) +
+  geom_point(mapping = aes(x=A_Grasses, y=log(BSNE_gM2day+.1), col=Site), position="jitter") #Might be interesting for SM, but WM just doesn't have any grasses growing
+
+ggplot(data=BSNE_dat) +
+  geom_point(mapping = aes(x=A_Grasses, y=BSNE_gM2day, col=Site), position="jitter")
+
+
+ggplot(data=BSNE_dat) +
+  geom_point(mapping = aes(x=Chla, y=Treatment_Year, col=Site), position="jitter") #Chla should be correlation to years of recovery, but it isn't closely..
+ggplot(data=BSNE_dat) +
+  geom_point(mapping = aes(x=Chla, y=LightCrust, col=Site), position="jitter") #Okay, this relationship is more as I'd expect; positive linear relationship between light crust and chl a 
+ggplot(data=BSNE_dat) +
+  geom_point(mapping = aes(x=Chla, y=DarkCrust, col=Site), position="jitter") #Less of a correlation with dark crust
+ggplot(data=BSNE_dat) +
+  geom_point(mapping = aes(x=Lichen, y=DarkCrust, col=Site), position="jitter") #Dark crust also isn't as correlated to moss or lichen as I'd expect
+
+#Exploring some more ground cover variables
+ggplot(data=BSNE_dat) +
+  geom_point(mapping = aes(x=Litter, y=log(BSNE_gM2day+.1), col=Site), position="jitter") #This is surprising! This relationship looks pretty good/negative for WM but not for SM at all!
+ggplot(data=BSNE_dat) +
+  geom_point(mapping = aes(x=Litter, y=log(BSNE_gM2day+.1), col=Treatment), position="jitter") #Lots of litter for B, followed by P and C. L had no litter and high erosion!
+
+ggplot(data=BSNE_dat) +
+  geom_point(mapping = aes(x=LightCrust, y=log(BSNE_gM2day+.1), col=Site), position="jitter")#This looks important, especially for SM!
+
+ggplot(data=BSNE_dat) +
+  geom_point(mapping = aes(x=DarkCrust, y=log(BSNE_gM2day+.1), col=Site), position="jitter") #Less important but still looks like there's a negative relationship
+
+ggplot(data=BSNE_dat) +
+  geom_point(mapping = aes(x=Soil, y=log(BSNE_gM2day+.1), col=Site), position="jitter") #Positive relationship, especially for SM!
+ggplot(data=BSNE_dat) +
+  geom_point(mapping = aes(x=Soil, y=log(BSNE_gM2day+.1), col=Treatment), position="jitter") #Much more bare soil for L and P than B and C! Looks positive!
+
+
+ggplot(data=BSNE_dat) +
+  geom_point(mapping = aes(x=Wood, y=log(BSNE_gM2day+.1), col=Site), position="jitter") #Way more wood at WM than SM. possible negative relationship for WM
+
+ggplot(data=BSNE_dat) +
+  geom_point(mapping = aes(x=Burnt_RootShrub, y=log(BSNE_gM2day+.1), col=Site), position="jitter")#Not much here; lot's of zeros...
+ggplot(data=BSNE_dat) +
+  geom_point(mapping = aes(x=Burnt_RootShrub, y=BSNE_gM2day, col=Site), position="jitter")#Not much here; lot's of zeros...
+
+
+ggplot(data=BSNE_dat) +
+  geom_point(mapping = aes(x=Burnt_PileSoil, y=log(BSNE_gM2day+.1), col=Site), position="jitter") 
+ggplot(data=BSNE_dat) +
+  geom_point(mapping = aes(x=Burnt_PileSoil, y=log(BSNE_gM2day+.1), col=Treatment), position="jitter") #This is kind of hard to interpret, and doesn't look super significant, but it does break up by Treatment very well.
+ggplot(data=BSNE_dat) +
+  geom_point(mapping = aes(x=Burnt_PileSoil, y=BSNE_gM2day, col=Site), position="jitter") 
+ggplot(data=BSNE_dat) +
+  geom_point(mapping = aes(x=Burnt_PileSoil, y=BSNE_gM2day, col=Treatment), position="jitter") 
